a8fe00792512eea64b9f3ade431d3f7f34afe096
Kanmani Priyadharshini <kanmanisri2020@gmail.com>
2023-10-06 18:17:48 +0530

    added coupon window and dark mode

--- scripts/home.js
+++ scripts/home.js
@@ -1,3 +1,38 @@
+let dark=false;
+window.onload = onLoadPage();
+
+// const couponTimeOut = setTimeout(closeCoupon, 10000);
+function onLoadPage(){
+    document.getElementById("coupon").style.visibility = 'visible';
+    document.getElementById("downloadFrame").style.opacity = '0.5';
+    document.getElementById("topCategoriesSection").style.opacity = '0.5';
+}
+
+function closeCoupon(){
+    document.getElementById("coupon").style.visibility = 'hidden';
+    document.getElementById("downloadFrame").style.opacity = '1';
+    document.getElementById("topCategoriesSection").style.opacity = '1';
+}
+
+const changeMode = () => {    
+    let myBody = document.body;
+    myBody.classList.toggle('darkMode');
+    // myBody.classList.toggle('heading');
+    console.log(document.getElementById('darkModeButton'));
+    if(!dark){
+        document.getElementById('darkModeButton').classList.remove('fa-moon');
+        document.getElementById('darkModeButton').classList.add('fa-sun');
+        document.getElementById('darkModeButton').innerText = ' Light Mode';
+        dark=true;
+    }
+    else{
+        document.getElementById('darkModeButton').classList.remove('fa-sun');
+        document.getElementById('darkModeButton').classList.add('fa-moon');
+        document.getElementById('darkModeButton').innerText = ' Dark Mode';
+        dark=false;
+    }    
+}
+
 function showMore() {
     // var dots = document.getElementById("dots");
     
