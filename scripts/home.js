15156f15cadd90091422bf304c548e7650bb154c
Kanmani Priyadharshini <kanmanisri2020@gmail.com>
2023-10-18 19:39:50 +0530

    added some more responsiveness to the web page

--- scripts/home.js
+++ scripts/home.js
@@ -84,13 +84,43 @@ const changeMode = () => {
     let myBody = document.body;
     myBody.classList.toggle('darkMode');
     // myBody.classList.toggle('heading');
-    console.log(document.getElementById('darkModeButton'));
     if(!dark){
         document.getElementById('darkModeButton').classList.remove('fa-moon');
         document.getElementById('darkModeButton').classList.add('fa-sun');
         document.getElementById('darkModeButton').innerText = ' Light Mode';
         document.getElementById("displayLocality").style.color = 'white';
-        document.getElementById("coupon").style.backgroundImage = 'linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.4)), url(https://i.ibb.co/JR9w6GH/meesho-Coupon.jpg)';        
+        document.getElementById("coupon").style.backgroundImage = 'linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.4)), url(https://i.ibb.co/JR9w6GH/meesho-Coupon.jpg)';
+        document.getElementById("downloadFrame").style.opacity = '0.7';
+        document.getElementById("topCategoriesSection").style.opacity = '0.7';
+        document.getElementById("partner").style.opacity = '0.7';
+        for(i of document.getElementsByClassName("form-floating"))
+        {
+            i.getElementsByTagName("select")[0].style.backgroundColor = "slategrey";
+            i.getElementsByTagName("select")[0].style.color = "white";
+            for(j of i.getElementsByTagName("select")[0].getElementsByTagName("option")){
+                j.style.backgroundColor = "lightsteelblue";
+            }
+        }
+        for(i of document.getElementsByClassName("accordion-header"))
+        {
+            for(j of i.getElementsByTagName("button")){
+                j.style.backgroundColor = 'slategrey';                
+            }
+        }
+        for(i of document.getElementsByClassName("accordBody"))
+        {
+            i.style.backgroundColor = 'lightsteelblue';
+            for(j of i.getElementsByTagName("div")){
+                for(k of j.getElementsByTagName("label")){
+                    k.style.color = 'white';
+                }
+            }
+        }
+        for(i of document.getElementsByClassName("plainButton"))
+        {
+            i.style.color = 'white';
+        }
+        document.getElementById("leftFilterFrame").style.backgroundColor = '#4c4c4c';
         dark=true;
     }
     else{
@@ -99,6 +129,37 @@ const changeMode = () => {
         document.getElementById('darkModeButton').innerText = ' Dark Mode';
         document.getElementById("displayLocality").style.color = 'black';
         document.getElementById("coupon").style.backgroundImage = 'url(https://i.ibb.co/JR9w6GH/meesho-Coupon.jpg)';
+        document.getElementById("downloadFrame").style.opacity = '1';
+        document.getElementById("topCategoriesSection").style.opacity = '1';
+        document.getElementById("partner").style.opacity = '1';
+        for(i of document.getElementsByClassName("form-floating"))
+        {
+            i.getElementsByTagName("select")[0].style.backgroundColor = "white";
+            i.getElementsByTagName("select")[0].style.color = "grey";
+            for(j of i.getElementsByTagName("select")[0].getElementsByTagName("option")){
+                j.style.backgroundColor = "white";
+            }
+        }
+        for(i of document.getElementsByClassName("accordion-header"))
+        {
+            for(j of i.getElementsByTagName("button")){
+                j.style.backgroundColor = 'white';                
+            }
+        }
+        for(i of document.getElementsByClassName("accordBody"))
+        {
+            i.style.backgroundColor = 'white';
+            for(j of i.getElementsByTagName("div")){
+                for(k of j.getElementsByTagName("label")){
+                    k.style.color = 'grey';
+                }
+            }
+        }
+        for(i of document.getElementsByClassName("plainButton"))
+        {
+            i.style.color = 'grey';
+        }
+        document.getElementById("leftFilterFrame").style.backgroundColor = 'white';
         dark=false;
     }    
 }
